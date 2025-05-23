91265c2d4ce1105945293d818f1d5b766136ed58
Engelbert <engelfreites97@gmail.com>
2025-05-22 21:26:15 -0300

    feat: Create OutServices component

---
+++ src/data/ourservices.js
@@ -0,0 +1,162 @@
+export const OURSERVICES = [
+  {
+    title: "Despigmentación Corporal",
+    aboutServices:
+      "La despigmentación corporal se realiza para aclarar áreas oscuras de la piel, unificar el tono cutáneo y tratar condiciones como hiperpigmentación, manchas o melasma, mejorando así la apariencia estética de la piel.",
+    price: "0$",
+    time: "1 hora",
+    img: "https://images.unsplash.com/photo-1555820585-c5ae44394b79?q=80&w=1450&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    title: "Exfoliación Corporal",
+    aboutServices:
+      "La exfoliación corporal se realiza para eliminar células muertas de la piel, mejorar su textura, estimular la regeneración celular y dejarla más suave, luminosa y receptiva a tratamientos posteriores.",
+    price: "0$",
+    time: "1 hora",
+    img: "https://images.unsplash.com/photo-1555820585-c5ae44394b79?q=80&w=1450&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    title: "Hidrolipoclasia",
+    aboutServices:
+      "Tratamiento no quirúrgico que elimina grasa localizada mediante la infiltración de solución salina y ultrasonido, rompiendo las células grasas para su eliminación a través del sistema linfático. Es una opción menos invasiva que la liposucción.",
+    price: "0$",
+    time: "1 hora 30min",
+    img: "https://images.unsplash.com/photo-1555820585-c5ae44394b79?q=80&w=1450&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    title: "Depilación definitiva con Láser Diodo Soprano",
+    aboutServices:
+      "La depilación definitiva con láser diodo Soprano sirve para eliminar el vello no deseado de forma progresiva y duradera. Este tipo de láser actúa destruyendo el folículo piloso sin dañar la piel, siendo eficaz en diversos tipos de piel y vello, y ofreciendo un tratamiento prácticamente indoloro gracias a su tecnología de enfriamiento.",
+    price: "0$",
+    time: "Dependiendo la zona",
+    img: "https://images.unsplash.com/photo-1555820585-c5ae44394b79?q=80&w=1450&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    title: "Borrado de Tatuaje",
+    aboutServices:
+      "La eliminación de tatuajes con láser sirve para borrar o atenuar tatuajes no deseados. El láser fragmenta las partículas de tinta en la piel, permitiendo que el cuerpo las reabsorba y elimine gradualmente a través del sistema linfático. Es un procedimiento seguro y eficaz, aunque requiere varias sesiones según el tipo de tinta y la profundidad del tatuaje.",
+    price: "0$",
+    time: "No especificado",
+    img: "https://images.unsplash.com/photo-1555820585-c5ae44394b79?q=80&w=1450&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    title: "Láser CO2 Fraccionado",
+    aboutServices:
+      "El láser CO2 fraccionado sirve para rejuvenecer la piel, mejorar su textura y tratar imperfecciones como arrugas, cicatrices de acné, manchas y poros dilatados. Funciona estimulando la producción de colágeno mediante la vaporización controlada de capas superficiales de la piel, logrando una regeneración más firme, lisa y uniforme.",
+    price: "0$",
+    time: "1 hora 30min",
+    img: "https://images.unsplash.com/photo-1555820585-c5ae44394b79?q=80&w=1450&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    title: "Subsicion",
+    aboutServices:
+      "La subcisión en tratamientos para secuelas de acné sirve para liberar las fibras de tejido fibroso que tiran hacia abajo la piel, causando hundimientos o cicatrices. Al romper estas fibras, se mejora el relieve de la piel, suavizando las cicatrices y favoreciendo su reparación.",
+    price: "0$",
+    time: "1 hora",
+    img: "https://images.unsplash.com/photo-1555820585-c5ae44394b79?q=80&w=1450&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    title: "Limpieza Facial profunda",
+    aboutServices:
+      "Una limpieza facial profunda sirve para eliminar impurezas, exceso de grasa, células muertas y puntos negros, purificando los poros y mejorando la textura y apariencia de la piel, dejándola fresca, saludable y preparada para otros tratamientos.",
+    price: "0$",
+    time: "1 hora",
+    img: "https://images.unsplash.com/photo-1555820585-c5ae44394b79?q=80&w=1450&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    title: "Despigmentación Facial",
+    aboutServices:
+      "La despigmentación facial sirve para reducir manchas oscuras, unificar el tono de la piel y tratar condiciones como melasma o hiperpigmentación, logrando una piel más luminosa y uniforme.",
+    price: "0$",
+    time: "1 hora",
+    img: "https://images.unsplash.com/photo-1555820585-c5ae44394b79?q=80&w=1450&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    title: "Tratamiento Anti Acné",
+    aboutServices:
+      "Un tratamiento antiacné sirve para controlar y reducir la inflamación, eliminar bacterias que causan el acné, regular la producción de grasa y prevenir la aparición de brotes, mejorando la salud y apariencia de la piel.",
+    price: "0$",
+    time: "6 sesiones de 1 hora cada una",
+    img: "https://images.unsplash.com/photo-1555820585-c5ae44394b79?q=80&w=1450&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    title: "Tratamientos anti envejecimiento",
+    aboutServices:
+      "Un tratamiento anti envejecimiento sirve para prevenir y reducir signos visibles del envejecimiento, como arrugas, pérdida de firmeza y manchas, estimulando la producción de colágeno y mejorando la hidratación y textura de la piel para una apariencia más joven y saludable.",
+    price: "0$",
+    time: "1 hora",
+    img: "https://images.unsplash.com/photo-1555820585-c5ae44394b79?q=80&w=1450&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    title: "Dermaplaning",
+    aboutServices:
+      "El dermaplaning sirve para exfoliar la piel de manera suave, eliminando células muertas y el vello fino (vellus), lo que deja la piel más suave, luminosa y preparada para absorber mejor los tratamientos tópicos.",
+    price: "0$",
+    time: "1 hora",
+    img: "https://images.unsplash.com/photo-1555820585-c5ae44394b79?q=80&w=1450&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    title: "Microdermoabrasión",
+    aboutServices:
+      "La microdermoabrasión sirve para exfoliar y renovar la capa superficial de la piel, mejorando su textura, reduciendo poros, manchas y líneas finas, y estimulando la regeneración celular para una piel más suave y uniforme.",
+    price: "0$",
+    time: "1 hora",
+    img: "https://images.unsplash.com/photo-1555820585-c5ae44394b79?q=80&w=1450&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    title: "Punta de diamante",
+    aboutServices:
+      "El tratamiento con punta de diamante es una forma de microdermoabrasión que elimina células muertas y mejora la textura de la piel. Ayuda a reducir arrugas, cicatrices, manchas y poros abiertos, promoviendo una piel más suave y rejuvenecida.",
+    price: "0$",
+    time: "1 hora",
+    img: "https://images.unsplash.com/photo-1555820585-c5ae44394b79?q=80&w=1450&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    title: "Plasma Rico en Plaquetas (PRP)",
+    aboutServices:
+      "El PRP se utiliza para rejuvenecimiento facial, capilar y cicatrización. Estimula la producción de colágeno, mejora la textura de la piel, reduce arrugas y fortalece los folículos capilares para combatir la caída del cabello.",
+    price: "0$",
+    time: "1 hora 30min",
+    img: "https://images.unsplash.com/photo-1555820585-c5ae44394b79?q=80&w=1450&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    title: "Microneedling",
+    aboutServices:
+      "El microneedling consiste en microperforaciones que estimulan la producción de colágeno y elastina, ayudando a mejorar cicatrices, arrugas, poros dilatados y firmeza de la piel.",
+    price: "0$",
+    time: "1 hora 30min",
+    img: "https://images.unsplash.com/photo-1555820585-c5ae44394b79?q=80&w=1450&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    title: "Terapia Capilar",
+    aboutServices:
+      "La terapia capilar sirve para fortalecer el cuero cabelludo, prevenir la caída del cabello, estimular su crecimiento y mejorar su salud general mediante la aplicación de productos, masajes y tratamientos como mesoterapia o láser.",
+    price: "0$",
+    time: "1 hora",
+    img: "https://images.unsplash.com/photo-1555820585-c5ae44394b79?q=80&w=1450&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    title: "Tratamiento para manchas",
+    aboutServices:
+      "Este tratamiento sirve para atenuar o eliminar manchas oscuras en la piel causadas por el sol, envejecimiento, acné u otras causas, logrando un tono más uniforme y una piel más clara.",
+    price: "0$",
+    time: "1 hora",
+    img: "https://images.unsplash.com/photo-1555820585-c5ae44394b79?q=80&w=1450&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    title: "Rellenos faciales con ácido hialurónico",
+    aboutServices:
+      "Los rellenos con ácido hialurónico sirven para restaurar volumen facial perdido, suavizar arrugas y líneas de expresión, y mejorar la hidratación de la piel, logrando un aspecto más juvenil y natural.",
+    price: "0$",
+    time: "1 hora",
+    img: "https://images.unsplash.com/photo-1555820585-c5ae44394b79?q=80&w=1450&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    title: "Toxina Botulínica (Botox)",
+    aboutServices:
+      "La toxina botulínica se aplica para relajar temporalmente los músculos faciales que causan arrugas dinámicas, como las líneas de expresión en la frente y alrededor de los ojos, logrando un rostro más liso y rejuvenecido.",
+    price: "0$",
+    time: "1 hora",
+    img: "https://images.unsplash.com/photo-1555820585-c5ae44394b79?q=80&w=1450&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+];

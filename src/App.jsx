24b04f65077e25212ec19acff664cfceda8f84fd
Engelbert <engelfreites97@gmail.com>
2025-05-29 22:19:01 -0300

    feat: implement comment components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Hero from "./assets/components/Hero/Hero";
 import AboutUs from "./components/AboutUs/AboutUs";
+import Comments from "./components/Comments/Comments";
 import OurServices from "./components/OurServices/OurServices";
 import We from "./components/We/We";
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <AboutUs />
       <OurServices />
       <We />
+      <Comments />
     </>
   );
 }
